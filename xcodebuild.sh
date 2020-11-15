a53afd1319a68d65af7fa508612f9e84292841f3
kongdelu <kongdelu2009@yeah.net>
2020-11-16 00:14:09 +0800

    Update xcodebuild.sh

--- xcodebuild.sh
+++ xcodebuild.sh
@@ -1,86 +1,45 @@
 #注意：脚本目录和xxxx.xcworkspace要在同一个目录，如果放到其他目录，请自行修改脚本。
 #工程名字(Target名字)
-Project_Name="xxx"
+Project_Name="xxxx"
 #配置环境，Release或者Debug
 Configuration="Release"
-
 #AdHoc版本的Bundle ID
-AdHocBundleID="com.xxx"
-
-#AppStore版本的Bundle ID
-AppStoreBundleID="com.xxxxx"
-
-#enterprise的Bundle ID
-EnterpriseBundleID="com.xxxxx"
+AdHocBundleID="com.xx.xx"
 
 # ADHOC
 #证书名#描述文件
-ADHOCCODE_SIGN_IDENTITY="iPhone Distribution: xxx (xxx)"
-ADHOCPROVISIONING_PROFILE_NAME="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-
-#AppStore证书名#描述文件
-APPSTORECODE_SIGN_IDENTITY="iPhone Distribution: xxx (xxx)"
-APPSTOREROVISIONING_PROFILE_NAME="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-
-#企业(enterprise)证书名#描述文件
-ENTERPRISECODE_SIGN_IDENTITY="iPhone Distribution: xxx (xxx)"
-ENTERPRISEROVISIONING_PROFILE_NAME="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+ADHOCCODE_SIGN_IDENTITY="iPhone Distribution: xx xx (xxxx)"
+ADHOCPROVISIONING_PROFILE_NAME="xxxx-xxx-xxx-xxx-xxxx"
 
 #加载各个版本的plist文件
 AdhocExportOptionsPlist=./adhoc_export_option.plist
-AppStoreExportOptionsPlist=./appstore_export_option.plist
-EnterpriseExportOptionsPlist=./enterprise_export_option.plist
 
 AdhocExportOptionsPlist=${AdhocExportOptionsPlist}
-AppStoreExportOptionsPlist=${AppStoreExportOptionsPlist}
-EnterpriseExportOptionsPlist=${EnterpriseExportOptionsPlist}
 
-echo "====================="
-echo "    1 Appstore"
-echo "    2 Adhoc"
-echo "    3 Enterprise"
-echo "====================="
-## 读取用户输入并存到变量里
-read -p "选择打包方式(输入序号):" num
-sleep 0.5
-method=$num
-# 判读用户是否有输入
-if [ ! -n "$method" ];then
-    echo "exit"
-    exit
-fi
+echo "Clean..."
+rm -rf ipa_archive
+rm -rf build/
+xcodebuild -quiet clean -workspace ./$Project_Name.xcworkspace -scheme $Project_Name -configuration $Configuration
 
-if [ $method == 1 ]
-then
-   echo "Clean..."
-   rm -rf ~/Desktop/$Project_Name-adhoc.ipa
-   rm -rf build/
-   xcodebuild -quiet clean -workspace ./$Project_Name.xcworkspace -scheme $Project_Name -configuration $Configuration
-   echo "===================== Appstore 打包 ====================="
-   xcodebuild -quiet -project $Project_Name.xcworkspace -scheme $Project_Name -configuration $Configuration -archivePath build/$Project_Name-appstore.xcarchive clean archive build  CODE_SIGN_IDENTITY="${APPSTORECODE_SIGN_IDENTITY}" PROVISIONING_PROFILE="${APPSTOREROVISIONING_PROFILE_NAME}" PRODUCT_BUNDLE_IDENTIFIER="${AppStoreBundleID}"
-   xcodebuild -quiet -exportArchive -archivePath build/$Project_Name-appstore.xcarchive -exportOptionsPlist $AppStoreExportOptionsPlist -exportPath ~/Desktop/$Project_Name-appstore.ipa
-
-elif [ $method == 2 ]
-then
-   echo "Clean..."
-   rm -rf ~/Desktop/$Project_Name-adhoc.ipa
-   rm -rf build/
-   xcodebuild -quiet clean -workspace ./$Project_Name.xcworkspace -scheme $Project_Name -configuration $Configuration
-   echo "===================== Adhoc 打包 ====================="
-   xcodebuild -quiet -workspace $Project_Name.xcworkspace -scheme $Project_Name -configuration $Configuration -archivePath build/$Project_Name-adhoc.xcarchive clean archive build CODE_SIGN_IDENTITY="${ADHOCCODE_SIGN_IDENTITY}" PROVISIONING_PROFILE="${ADHOCPROVISIONING_PROFILE_NAME}" PRODUCT_BUNDLE_IDENTIFIER="${AdHocBundleID}"
-   xcodebuild -quiet -exportArchive -archivePath build/$Project_Name-adhoc.xcarchive -exportOptionsPlist $AdhocExportOptionsPlist -exportPath ~/Desktop/$Project_Name-adhoc.ipa
-
-elif [ $method == 3 ]
-then
-    echo "Clean..."
-    rm -rf ~/Desktop/$Project_Name-adhoc.ipa
-    rm -rf build/
-    xcodebuild -quiet clean -workspace ./$Project_Name.xcworkspace -scheme $Project_Name -configuration $Configuration
-    echo "===================== Enterprise 打包 ====================="
-    xcodebuild -quiet -project $Project_Name.xcworkspace -scheme $Project_Name -configuration $Configuration -archivePath build/$Project_Name-enterprise.xcarchive clean archive build CODE_SIGN_IDENTITY="${ENTERPRISECODE_SIGN_IDENTITY}" PROVISIONING_PROFILE="${ENTERPRISEROVISIONING_PROFILE_NAME}" PRODUCT_BUNDLE_IDENTIFIER="${EnterpriseBundleID}"
-    xcodebuild -quiet -exportArchive -archivePath build/$Project_Name-enterprise.xcarchive -exportOptionsPlist $EnterpriseExportOptionsPlist -exportPath ~/Desktop/$Project_Name-enterprise.ipa
-
+echo "Adhoc 打包..."
+xcodebuild -quiet -workspace $Project_Name.xcworkspace -scheme $Project_Name -configuration $Configuration -archivePath build/$Project_Name-adhoc.xcarchive clean archive build CODE_SIGN_IDENTITY="${ADHOCCODE_SIGN_IDENTITY}" PROVISIONING_PROFILE="${ADHOCPROVISIONING_PROFILE_NAME}" PRODUCT_BUNDLE_IDENTIFIER="${AdHocBundleID}"
+if [ $? -eq 0 ]; then
+    xcodebuild -quiet -exportArchive -archivePath build/$Project_Name-adhoc.xcarchive -exportOptionsPlist $AdhocExportOptionsPlist -exportPath ipa_archive
+    if [ $? -eq 0 ]; then
+        echo "上传蒲公英..."
+        curl -F 'file=@ipa_archive/Runner.ipa' -F '_api_key=xxxx' https://www.pgyer.com/apiv2/app/upload
+        if [ $? -eq 0 ]; then
+            echo -e "\n\n\033[5;32m 上传成功！\033[0m\n"
+            sleep 0.5
+            rm -rf ipa_archive
+            rm -rf build/
+        else
+            echo -e "\n\033[31m 上传失败！\033[0m\n"
+        fi
+	 
+    else
+        echo -e "\n\033[31m 导出失败！ \033[0m\n"
+    fi
 else
-   echo "无效序号..."
-   exit 1
+    echo -e "\n\033[31m 打包失败！ \033[0m\n"
 fi
